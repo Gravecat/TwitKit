af026c37e0658f0b735a3ee40b7cf481a94e0c7d
Raine Simmons <gc@gravecat.com>
2022-05-01 22:29:42 +0100

    twitter_random.py can now link directly to a tweets-only page, or view the normal page with tweets + retweets, and toggle between the two during runtime (#3)

--- twitter_random.py
+++ twitter_random.py
@@ -5,16 +5,27 @@ import os
 import random
 import webbrowser
 
+tweets_only = True  # Set this to False to default to visiting the friend's page directly.
+
 friends = open(os.path.dirname(__file__) + '\\twitter_following.txt', 'r')
 lines = friends.readlines()
 friends.close()
 
-print('Press Enter to visit a random friend, or anything else (then enter) to exit.')
+print('Press Enter to visit a random friend, Q (then enter) to exit, or T to toggle tweets-only (no retweets) mode.')
+print('Tweets-only mode is currently', 'enabled.' if tweets_only else 'disabled.')
 while True:
     key = input()
     if (len(key)):
-        print('Goodbye!')
-        exit()
-    fren = random.choice(lines).strip('\n')
-    print('Visiting ' + fren + '!')
-    webbrowser.open('https://twitter.com/' + fren)
+        if (key[0] == 'q' or key[0] == 'Q'):
+            print('Goodbye!')
+            exit()
+        elif (key[0] == 't' or key[0] == 'T'):
+            tweets_only = not tweets_only
+            print('Tweets-only mode is currently', 'enabled.' if tweets_only else 'disabled.')
+        else:
+            print('Press Enter to visit a random friend, Q (then enter) to exit, or T to toggle tweets-only (no retweets) mode.')
+    else:
+        fren = random.choice(lines).strip('\n')
+        print('Visiting ' + fren + '!')
+        if tweets_only: webbrowser.open('https://twitter.com/search?q=from%3A' + fren + '&src=typed_query&f=live')
+        else: webbrowser.open('https://twitter.com/' + fren)
