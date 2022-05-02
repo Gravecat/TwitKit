4a23b524d41ca9c2a5020ec65f6ac929695aa822
Raine Simmons <gc@gravecat.com>
2022-05-02 16:18:17 +0100

    Reformatted tweets_only.py to use main() function.

--- tweets_only.py
+++ tweets_only.py
@@ -9,17 +9,20 @@ def tweets_only(user):
     print('Opening tweets-only search page for', user)
     webbrowser.open('https://twitter.com/search?q=from%3A' + user + '&src=typed_query&f=live')
 
-if len(sys.argv) > 1:
-    args = sys.argv
-    args.pop(0)
-    for arg in args:
-        tweets_only(arg)
-    exit()
-
-print('Enter the username of a Twitter user to view their tweets only, or a blank line to exit.')
-while True:
-    user = input()
-    if not len(user):
-        print('Goodbye!')
+def main():
+    if len(sys.argv) > 1:
+        args = sys.argv
+        args.pop(0)
+        for arg in args:
+            tweets_only(arg)
         exit()
-    tweets_only(user)
+
+    print('Enter the username of a Twitter user to view their tweets only, or a blank line to exit.')
+    while True:
+        user = input()
+        if not len(user):
+            print('Goodbye!')
+            exit()
+        tweets_only(user)
+
+if __name__ == '__main__': main()
